062b90bdceca1470b135b3cb0ce7ec3b0d7380f4
Ali Daghighi <daghighi1376@gmail.com>
2017-12-27 23:23:41 +0330

    Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 """
 Main files
 """
-from DS import LinkedList
-
 
 print(30 * ' ' + 20 * '*')
 print('Type "exit" when you wanted to quit.')
@@ -19,52 +17,50 @@ while True:
             break
         elif 'help' in _input[0]:
             print(30 * ' ' + 20 * '*')
-            print('RUN command:')
-            print('>>> RUN LinkedList Quick ')
-            print('>>> RUN LinkedList Insertion ')
-            print('>>> RUN LinkedList Merge ')
-            print('>>> RUN LinkedList Bubble ')
-            print('>>> RUN LinkedList Optimum Insertion N ')
-            print('>>> RUN LinkedList Optimum Bubble N ')
-            print('>>> RUN Matrix Quick ')
-            print('>>> RUN Matrix Insertion  ')
-            print('>>> RUN Matrix Merge ')
-            print('>>> RUN Matrix Bubble ')
-            print('>>> RUN Matrix Optimum Insertion N ')
-            print('>>> RUN Matrix Optimum Bubble N ')
+            print('run command:')
+            print('    >>> run LinkedList Quick ')
+            print('    >>> run LinkedList Insertion ')
+            print('    >>> run LinkedList Merge ')
+            print('    >>> run LinkedList Bubble ')
+            print('    >>> run LinkedList Optimum Insertion N ')
+            print('    >>> run LinkedList Optimum Bubble N ')
+            print('    >>> run Matrix Quick ')
+            print('    >>> run Matrix Insertion  ')
+            print('    >>> run Matrix Merge ')
+            print('    >>> run Matrix Bubble ')
+            print('    >>> run Matrix Optimum Insertion N ')
+            print('    >>> run Matrix Optimum Bubble N ')
 
         elif _input[1] == 'LinkedList':
-            print("DO1")
-            if _input[2] in 'Quick':
+            if _input[2] in 'Quick' and len(_input) == 3:
                 print("DO2")
-            elif _input[2] in 'Insertion':
+            elif _input[2] in 'Insertion' and len(_input) == 3:
                 print("DO3")
-            elif _input[2] in 'Merge':
+            elif _input[2] in 'Merge' and len(_input) == 3:
                 print("DO4")
-            elif _input[2] in 'Bubble':
+            elif _input[2] in 'Bubble' and len(_input) == 3:
                 print("DO5")
             elif _input[2] in 'Optimum':
-                if _input[3] in 'Insertion':
+                if _input[3] in 'Insertion' and len(_input) == 4:
                     print("DO6")
-                elif _input[3] in 'Bubble':
+                elif _input[3] in 'Bubble' and len(_input) == 4:
                     print("DO7")
                 else:
                     print('Wrong!\ncheck "help" for more information!')
 
         elif _input[1] == 'Matrix':
-            print("DO12")
-            if _input[2] in 'Quick':
+            if _input[2] in 'Quick' and len(_input) == 3:
                 print("DO22")
-            elif _input[2] in 'Insertion':
+            elif _input[2] in 'Insertion' and len(_input) == 3:
                 print("DO32")
-            elif _input[2] in 'Merge':
+            elif _input[2] in 'Merge' and len(_input) == 3:
                 print("DO42")
-            elif _input[2] in 'Bubble':
+            elif _input[2] in 'Bubble' and len(_input) == 3:
                 print("DO52")
             elif _input[2] in 'Optimum':
-                if _input[3] in 'Insertion':
+                if _input[3] in 'Insertion' and len(_input) == 4:
                     print("DO62")
-                elif _input[3] in 'Bubble':
+                elif _input[3] in 'Bubble' and len(_input) == 4:
                     print("DO72")
                 else:
                     print('Wrong!\ncheck "help" for more information!')
