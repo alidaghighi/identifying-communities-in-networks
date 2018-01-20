7b0b72640685c418d934d0b1cc26f9ac236f4e6f
Ali Daghighi <daghighi1376@gmail.com>
2018-01-20 15:02:24 +0330

    Sorts

--- sort_methods.py
+++ sort_methods.py
@@ -1,4 +1,4 @@
-def insertionSort(arr):
+def insertion_sort(arr):
  
     # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
@@ -9,44 +9,44 @@ def insertionSort(arr):
         # greater than key, to one position ahead
         # of their current position
         j = i-1
-        while j >=0 and key < arr[j] :
-                arr[j+1] = arr[j]
+        while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
                 j -= 1
-        arr[j+1] = key
+        arr[j + 1] = key
 
 
-        
 # Python program for implementation of Quicksort Sort
- 
+
 # This function takes last element as pivot, places
 # the pivot element at its correct position in sorted
 # array, and places all smaller (smaller than pivot)
 # to left of pivot and all greater elements to right
 # of pivot
-def partition(arr,low,high):
-    i = ( low-1 )         # index of smaller element
+def partition(arr, low, high):
+    i = (low - 1)         # index of smaller element
     pivot = arr[high]     # pivot
  
-    for j in range(low , high):
+    for j in range(low, high):
  
         # If current element is smaller than or
         # equal to pivot
-        if   arr[j] <= pivot:
+        if arr[j] <= pivot:
          
             # increment index of smaller element
-            i = i+1
-            arr[i],arr[j] = arr[j],arr[i]
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
  
-    arr[i+1],arr[high] = arr[high],arr[i+1]
-    return ( i+1 )
- 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i + 1
+
+
 # The main function that implements QuickSort
 # arr[] --> Array to be sorted,
 # low  --> Starting index,
 # high  --> Ending index
- 
+
 # Function to do Quick sort
-def quickSort(arr,low,high):
+def quick_sort(arr,low,high):
     if low < high:
  
         # pi is partitioning index, arr[p] is now
@@ -55,11 +55,10 @@ def quickSort(arr,low,high):
  
         # Separately sort elements before
         # partition and after partition
-        quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high)
+        quick_sort(arr, low, pi-1)
+        quick_sort(arr, pi+1, high)
         
-        
-        
+
 # Python program for implementation of heap Sort
 
 # To heapify subtree rooted at index i.
@@ -81,13 +80,14 @@ def heapify(arr, n, i):
 
     # Change root, if needed
     if largest != i:
-        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
 
         # Heapify the root.
         heapify(arr, n, largest)
 
+
 # The main function to sort an array of given size
-def heapSort(arr):
+def heap_sort(arr):
     n = len(arr)
 
     # Build a maxheap.
@@ -98,9 +98,9 @@ def heapSort(arr):
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]   # swap
         heapify(arr, i, 0)
-        
-        
- # Python program for implementation of MergeSort
+
+
+# Python program for implementation of MergeSort
 
 # Merges two subarrays of arr[].
 # First subarray is arr[l..m]
@@ -114,10 +114,10 @@ def merge(arr, l, m, r):
     R = [0] * (n2)
 
     # Copy data to temp arrays L[] and R[]
-    for i in range(0 , n1):
+    for i in range(0, n1):
         L[i] = arr[l + i]
 
-    for j in range(0 , n2):
+    for j in range(0, n2):
         R[j] = arr[m + 1 + j]
 
     # Merge the temp arrays back into arr[l..r]
@@ -125,7 +125,7 @@ def merge(arr, l, m, r):
     j = 0     # Initial index of second subarray
     k = l     # Initial index of merged subarray
 
-    while i < n1 and j < n2 :
+    while i < n1 and j < n2:
         if L[i] <= R[j]:
             arr[k] = L[i]
             i += 1
@@ -148,18 +148,19 @@ def merge(arr, l, m, r):
         j += 1
         k += 1
 
+
 # l is for left index and r is right index of the
 # sub-array of arr to be sorted
-def mergeSort(arr,l,r):
+def merge_sort(arr, l, r):
     if l < r:
 
         # Same as (l+r)/2, but avoids overflow for
         # large l and h
-        m = (l+(r-1))/2
+        m = (l + (r - 1)) / 2
 
         # Sort first and second halves
-        mergeSort(arr, l, m)
-        mergeSort(arr, m+1, r)
+        merge_sort(arr, l, m)
+        merge_sort(arr, m + 1, r)
         merge(arr, l, m, r)
 
 
@@ -167,7 +168,7 @@ def mergeSort(arr,l,r):
 
 # A function to do counting sort of arr[] according to
 # the digit represented by exp.
-def countingSort(arr, exp1):
+def counting_sort(arr, exp1):
 
     n = len(arr)
 
@@ -180,29 +181,30 @@ def countingSort(arr, exp1):
     # Store count of occurrences in count[]
     for i in range(0, n):
         index = (arr[i]/exp1)
-        count[ (index)%10 ] += 1
+        count[(index) % 10] += 1
 
     # Change count[i] so that count[i] now contains actual
     #  position of this digit in output array
-    for i in range(1,10):
+    for i in range(1, 10):
         count[i] += count[i-1]
 
     # Build the output array
-    i = n-1
-    while i>=0:
+    i = n - 1
+    while i >= 0:
         index = (arr[i]/exp1)
-        output[ count[ (index)%10 ] - 1] = arr[i]
-        count[ (index)%10 ] -= 1
+        output[count[(index) % 10] - 1] = arr[i]
+        count[(index) % 10] -= 1
         i -= 1
 
     # Copying the output array to arr[],
     # so that arr now contains sorted numbers
     i = 0
-    for i in range(0,len(arr)):
+    for i in range(0, len(arr)):
         arr[i] = output[i]
 
+
 # Method to do Radix Sort
-def radixSort(arr):
+def radix_sort(arr):
 
     # Find the maximum number to know number of digits
     max1 = max(arr)
@@ -212,7 +214,7 @@ def radixSort(arr):
     # where i is current digit number
     exp = 1
     while max1/exp > 0:
-        countingSort(arr,exp)
+        counting_sort(arr, exp)
         exp *= 10
 
 
@@ -220,7 +222,7 @@ def radixSort(arr):
  
 # The main function that sort the given string arr[] in 
 # alphabetical order
-def countSort(arr):
+def count_sort(arr):
  
     # The output character array that will have sorted arr
     output = [0 for i in range(256)]
